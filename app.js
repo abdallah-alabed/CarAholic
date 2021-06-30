0705e0ed9df6b12c8082c9d1b02b76c6bdd231ee
abdalabed <>
2021-06-30 12:46:07 +0000

    created the js file and linked it to the html file

---
+++ app.js
@@ -0,0 +1,15 @@
+/*  This js file is to create more dynamic to our repo. as we should allow the user to interact with us*/
+
+var username = prompt("Hello, what is your name?")
+alert("Thank you " + username + " For visiting our Website!")
+
+var choice = prompt("Do you like us to recommend a car for you?")
+ if (choice == null){alert("please enjoy our website")}
+else if (choice == "no" ) { alert ("you can check the cars featured on the home page, enjoy!")}
+else if (choice == "yes"){alert("we are still testing this feature, for now please enjoy the content!")}
+
+
+var q1 = confirm("do you like our website?");
+if (q1 == true) {alert("Thank you for the feedback!")} 
+else {alert("we are sorry to hear this, please tell us what is wrong in the RateUS link in the website")}
+
